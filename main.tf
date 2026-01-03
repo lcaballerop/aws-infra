7646cf88485d70a48a7a6480de3a5491b9516482
Luis Caballero <lcaballerop@uni.pe>
2026-01-02 23:30:45 -0300

    update terraform files

--- main.tf
+++ main.tf
@@ -2,20 +2,30 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux_2023" {
   most_recent = true
+  owners      = ["amazon"] # Official Amazon Linux
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["al2023-ami-2023.*-x86_64"]
   }
 
-  owners = ["099720109477"] # Canonical
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t3.micro"
 
   tags = {
     Name = "learn-terraform"
